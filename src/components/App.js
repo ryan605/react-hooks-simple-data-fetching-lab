9a756b8476abf9b062f38c923ae957695b2f4393
ryan605 <langatryan26@gmail.com>
2023-01-17 21:37:44 +0300

    added random photo

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React,{ useEffect, useState } from 'react';
+
+function App() {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      setImageUrl(data.message);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+        <p>Loading..</p>
+      {imageUrl && <img src={imageUrl} alt="Random Dog" />}
+    </div>
+  );
+}
+export default App
